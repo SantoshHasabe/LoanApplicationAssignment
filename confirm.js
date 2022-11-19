a50a249ba47a35205d906ea12c6406fcdbc9423f
SantoshHasabe <santoshhasabe567@gmail.com>
2022-11-19 18:38:23 +0530

    Update confirm.js
    Adding comments

--- confirm.js
+++ confirm.js
@@ -1,7 +1,7 @@
-let obj1=JSON.parse(localStorage.getItem('myobj'))
+let obj1=JSON.parse(localStorage.getItem('myobj')) // converting json data into object
 let name= obj1.fn;
-const first = name.split(' ')[0]
+const first = name.split(' ')[0] //taking first name of user
 
 let email=obj1.em;
-document.getElementById('name').innerText=first;
+document.getElementById('name').innerText=first;  
 document.getElementById('email').innerText=email;
